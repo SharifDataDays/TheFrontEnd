fa6bf0a4edc7fda501a4eb5b8d822b15b1a0fc09
alireza dizaji <alirezadizaji@yahoo.com>
2019-11-27 15:53:46 +0330

    nothing :)

---
+++ pages/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Parallax} from 'react-scroll-parallax'
+const home = () => {
+    return (
+        <div>
+            <Parallax>
+                <img src= {'../'}/>
+            </Parallax>
+        </div>
+    )
+}
