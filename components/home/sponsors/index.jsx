e559ca4c6cc156599cc330b5eff4c21a5c62893a
savaw <savaw97@gmail.com>
2019-12-16 18:13:45 +0330

    Change sponsors
    Add react-visibility-sensor

--- components/home/sponsors/index.jsx
+++ components/home/sponsors/index.jsx
@@ -1,7 +1,8 @@
 import _ from 'lodash';
-import React, { Component, createRef } from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Grid as G, Ref, Visibility, Sticky, Table } from 'semantic-ui-react';
+import { Grid as G } from 'semantic-ui-react';
+import VisibilitySensor from 'react-visibility-sensor';
 import Logo from './logo';
 
 const Grid = styled(G)`
@@ -9,44 +10,39 @@ const Grid = styled(G)`
   margin-bottom: 2rem !important;
 `;
 
-export default class Sponsors extends Component {
-  state = {
-    calculations: {
-      direction: 'none',
-      height: 0,
-      width: 0,
-      topPassed: false,
-      bottomPassed: false,
-      pixelsPassed: 0,
-      percentagePassed: 0,
-      topVisible: false,
-      bottomVisible: false,
-      fits: false,
-      passing: false,
-      onScreen: false,
-      offScreen: false,
-    },
-  };
-  contextRef = createRef();
+class Sponsors extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
 
-  handleUpdate = (e, { calculations }) => this.setState({ calculations });
+  onChange = (isVisible) => {
+    this.setState({
+      visible: isVisible,
+    });
+  };
 
   render() {
-    const fits = this.state.calculations.fits;
     return (
-      <Ref innerRef={this.contextRef}>
-        <Visibility onUpdate={this.handleUpdate}>
-          <Grid centered>
-            {_.map(this.props.logos, (logo, i) => {
-              return (
-                <Grid.Column textAlign="center" computer={2} tablet={3} mobile={14} key={i}>
-                  <Logo src={logo} height={60}/>
-                </Grid.Column>
-              );
-            })}
-          </Grid>
-        </Visibility>
-      </Ref>
+      <>
+        <VisibilitySensor onChange={this.onChange} offset={{top:-20, bottom:-20}}>
+          <div>
+            <Grid centered>
+              {_.map(this.props.logos, (logo, i) => {
+                return (
+                  <Grid.Column textAlign="center" computer={2} tablet={3} mobile={14} key={i}>
+                    <Logo src={logo} height={60} visible={this.state.visible} />
+                  </Grid.Column>
+                );
+              })}
+            </Grid>
+          </div>
+        </VisibilitySensor>
+      </>
     );
   }
 }
+
+export default Sponsors;
