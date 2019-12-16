12c6515a90cc12e0ad9f520deb0d3567ba1bf801
parsa alian <parsaalian0@gmail.com>
2019-12-16 18:05:03 +0330

    minor styling change

--- components/home/newTimeline.js
+++ components/home/newTimeline.js
@@ -2,43 +2,40 @@ import { VerticalTimeline, VerticalTimelineElement } from './timeline/index';
 import './timeline/VerticalTimeline.css';
 import './timeline/VerticalTimelineElement.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGem } from "@fortawesome/free-solid-svg-icons";
+import { faGem } from '@fortawesome/free-solid-svg-icons';
 
 const constructTimeline = (items) => {
-    return items.map((item, i) => {
-        return(
-            <VerticalTimelineElement
-            key={i}
-            contentStyle={{ background: '	#1EBDC1', color: '#003a42' }}
-            contentArrowStyle={{ borderRight: '7px solid #1EBDC1' }}
-            date={item.date}
-            iconStyle={{ background: '#1EBDC1', color: '#003a42' }}
-            icon={<FontAwesomeIcon icon={faGem} style={{fontSize : 20}}/>}
-          >
-            <div style={{direction:'rtl'}}>
-              <h3 className="vertical-timeline-element-title">{item.heading}</h3>
-              <p>{item.text}</p>
-            </div>
-            
-          </VerticalTimelineElement>
-        )
-    }) 
-}
+  return items.map((item, i) => {
+    return (
+      <VerticalTimelineElement
+        key={i}
+        contentStyle={{ background: '	#1EBDC1', color: '#003a42' }}
+        contentArrowStyle={{ borderRight: '7px solid #1EBDC1' }}
+        date={item.date}
+        iconStyle={{ background: '#1EBDC1', color: '#003a42' }}
+        icon={<FontAwesomeIcon icon={faGem} style={{ fontSize: 20 }} />}
+      >
+        <div style={{ direction: 'rtl' }}>
+          <h3 className="vertical-timeline-element-title">{item.heading}</h3>
+          <p>{item.text}</p>
+        </div>
+      </VerticalTimelineElement>
+    );
+  });
+};
 
 const Timeline = (props) => {
   return (
     <div
       style={{
-        width : '100%',
-        paddingTop : 50,
-        paddingBottom : 5,
-        backgroundColor : '#252525'
+        width: '100%',
+        paddingTop: 50,
+        paddingBottom: 5,
+        backgroundColor: '#252525',
       }}
     >
-      <div style = {{width : '80%' , margin : 'auto'}}>  
-        <VerticalTimeline layout='1-column'>
-          {constructTimeline(props.items)}
-        </VerticalTimeline>
+      <div style={{ width: '80%', margin: 'auto' }}>
+        <VerticalTimeline layout="1-column">{constructTimeline(props.items)}</VerticalTimeline>
       </div>
     </div>
   );
