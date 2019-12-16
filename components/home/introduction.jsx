f380226c0c6d7e91c7f4015f2a96b565ff17f5b8
parsa alian <parsaalian0@gmail.com>
2019-12-16 15:35:32 +0330

    added introduction section

---
+++ components/home/introduction.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Grid, Header as H, Image } from 'semantic-ui-react';
+
+const Header = styled(H)`
+  font-size: 3rem;
+`;
+
+const Text = styled.p`
+  font-size: 1.75rem;
+  line-height: 1.5;
+`;
+
+function Introduction({ header, content }) {
+  return (
+    <Grid stackable inverted centered>
+      <Grid.Row style={{ padding: '12rem 2rem', backgroundColor: '#252525', color: 'white' }}>
+        <Grid.Column width={5}>
+          <Image style={{ padding: '2rem' }} size="large" src="/images/logo.png" />
+        </Grid.Column>
+        <Grid.Column textAlign="center" verticalAlign="middle" width={9}>
+          <Header as="h1">{header}</Header>
+          <Text>{content}</Text>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+}
+
+export default Introduction;
