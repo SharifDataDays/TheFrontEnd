7e300cfb7aa4b8b13345b2c1609f15197a0ef998
parsa alian <parsaalian0@gmail.com>
2019-12-16 22:14:01 +0330

    theme removed from syntax

--- components/global/mdx/code.jsx
+++ components/global/mdx/code.jsx
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import React from 'react';
 import styled from 'styled-components';
 import SH from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const SyntaxHighlighter = styled(SH)`
   text-align: left !important;
@@ -10,11 +9,7 @@ const SyntaxHighlighter = styled(SH)`
 
 function CodeBlock({ children, className }) {
   const language = _.replace(className, /language-/, '');
-  return (
-    <SyntaxHighlighter language={language} style={dark}>
-      {children}
-    </SyntaxHighlighter>
-  );
+  return <SyntaxHighlighter language={language}>{children}</SyntaxHighlighter>;
 }
 
 export default CodeBlock;
