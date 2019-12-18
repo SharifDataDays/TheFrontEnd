f4863a79983df57edce8fd563e8f11fd48d50e11
HamedKh99 <goo.hamedacc@gmail.com>
2019-12-18 19:54:15 +0330

    tried to add transition

--- components/home/introduction.jsx
+++ components/home/introduction.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Grid, Header as H, Image } from 'semantic-ui-react';
+import VisibilitySensor from 'react-visibility-sensor';
+import { Grid, Header as H, Image, Transition } from 'semantic-ui-react';
 
 const Header = styled(H)`
   font-size: 3rem;
@@ -11,28 +12,48 @@ const Text = styled.p`
   line-height: 1.5;
 `;
 
-function Introduction({ header, content }) {
-  return (
-    <div style={{ position: 'relative' }}>
-      <Grid stackable inverted centered>
-        <Grid.Row
-          style={{
-            padding: '12rem 2rem',
-            backgroundColor: '#252525',
-            color: 'white',
-          }}
-        >
-          <Grid.Column textAlign="center" verticalAlign="middle" width={6}>
-            <Image style={{ padding: '2rem' }} size="large" src="/images/logo.png" />
-          </Grid.Column>
-          <Grid.Column textAlign="center" verticalAlign="middle" width={7}>
-            <Header as="h1">{header}</Header>
-            <Text>{content}</Text>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </div>
-  );
-}
+export default class Introduction extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: false,
+    };
+  }
 
-export default Introduction;
+  onChangeVisibility = (isVisible) => {
+    console.log(isVisible)
+    this.setState({ isVisible });
+  };
+
+  render() {
+    const { header, content } = this.props;
+    return (
+      <VisibilitySensor
+        onChange={this.onChangeVisibility}
+        offset={{ top: 150 }}
+      >
+        <div style={{ position: 'relative' }}>
+          <Grid stackable inverted centered>
+            <Grid.Row
+              style={{
+                padding: '12rem 2rem',
+                backgroundColor: '#252525',
+                color: 'white',
+              }}
+            >
+              <Transition visible={this.state.isVisible} animation="scale" duration={500}>
+                <Grid.Column textAlign="center" verticalAlign="middle" width={6}>
+                  <Image style={{ padding: '2rem' }} size="large" src="/images/logo.png" />
+                </Grid.Column>
+              </Transition>
+              <Grid.Column textAlign="center" verticalAlign="middle" width={7}>
+                <Header as="h1">{header}</Header>
+                <Text>{content}</Text>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
+      </VisibilitySensor>
+    );
+  }
+}
