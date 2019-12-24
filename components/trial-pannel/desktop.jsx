298782614c1b5e8454196cbd86c6e8ebed8fb109
pooyamoini <pooyamoini2000@gmail.com>
2019-12-25 00:41:20 +0330

    labels added

--- components/trial-pannel/desktop.jsx
+++ components/trial-pannel/desktop.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Segment, Image, Header as H } from 'semantic-ui-react';
+import { Segment, Image, Header as H, Label as Lb } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 const RowSegment = styled(Segment)`
   padding: 0 !important;
   display: flex;
   justify-content: flex-end;
-  border-right: 3px solid ${(props) => (props.done === true ? 'green' : 'red')} !important;
-  border-left: 3px solid ${(props) => (props.done === true ? 'green' : 'red')} !important;
 `;
 
 const Header = styled(H)`
@@ -15,6 +13,20 @@ const Header = styled(H)`
   font-family: IRANSans, 'Open Sans', 'Helvetica Neue', Arial, Helvetica, sans-serif;
   margin-right: 2rem !important;
   margin-top: 2rem !important;
+  width: 75% !important;
+  text-align: right;
+`;
+
+const Label = styled(Lb)`
+  margin-left: 0.2rem !important;
+`;
+
+const SegmentLabel = styled(Segment)`
+  display: flex !important;
+`;
+
+const Text = styled.p`
+  margin-bottom: 1rem;
 `;
 
 const SegmentExamplePiledSegments = () => (
@@ -23,20 +35,40 @@ const SegmentExamplePiledSegments = () => (
     raised
     style={{ width: '60vw', border: '0px solid', marginLeft: '20vw', display: 'flex' }}
   >
-    <RowSegment done>
-      <Header>(pre-requirements) پیش نیاز‌ها </Header>
+    <RowSegment>
+      <Header>
+        <Text>(pre-requirements) پیش نیاز‌ها</Text>
+        <SegmentLabel basic raised>
+          {' '}
+          <Label circular color="grey">
+            2
+          </Label>
+          <Label circular color="black">
+            1
+          </Label>
+          <Label circular color="violet">
+            3
+          </Label>
+        </SegmentLabel>
+      </Header>
       <Image src="/images/pre-requirements.jpg" size="small" inline />
     </RowSegment>
-    <RowSegment raised>
-      <Header>(Data Cleaning) تمیزکردن داده</Header>
+    <RowSegment>
+      <Header>
+        <Text>(Data Cleaning) تمیزکردن داده</Text>
+      </Header>
       <Image src="/images/data-cleaning.jpg" size="small" inline />
     </RowSegment>
     <RowSegment raised>
-      <Header> (Data Visulisation) مصورسازی داده</Header>
+      <Header>
+        <Text>(Data Visulisation) مصورسازی داده</Text>
+      </Header>
       <Image src="/images/data-visual.jpg" size="small" inline />
     </RowSegment>
     <RowSegment raised>
-      <Header>(Regression) رگرسیون</Header>
+      <Header>
+        <Text>(Regression) رگرسیون</Text>
+      </Header>
       <Image src="/images/linear-regression.jpeg" size="small" inline />
     </RowSegment>
   </Segment.Group>
