1d66c25e366bfafefb48344f079cd300f6c4599e
baharkhd <baharkh127@gmail.com>
2019-12-29 17:41:40 +0330

    timeline bug fixed but has lag

--- components/dashboard/main/milestones.jsx
+++ components/dashboard/main/milestones.jsx
@@ -47,33 +47,34 @@ export default class Timeline extends React.Component {
     super(props);
 
     this.state = {
-        curPhaseIdx: 2,
-        curIdx: 0,
-        previdx: -1,
-        curVisibality: true,
-        prevVisibality: false
-    }
+      curPhaseIdx: 2,
+      curIdx: 0,
+      prevIdx: -1,
+      curVisibality: true,
+      prevVisibality: false,
+    };
   }
 
   toggleCurVisibility = () => {
-      let preVisibality = this.state.curVisibality
-      this.setState({curVisibality: !preVisibality})
-    //   this.setState({visibality: prevVisibality})
-      console.log("curVisibality:", this.state.curVisibality)
-  }
+    let preVisibality = this.state.curVisibality;
+    this.setState({ curVisibality: !preVisibality });
+    console.log('curVisibality:', this.state.curVisibality);
+  };
 
   togglePreVisibility = () => {
-    let preVisibality = this.state.prevVisibality
-    this.setState({prevVisibality: !preVisibality})
-  //   this.setState({visibality: prevVisibality})
-    console.log("curVisibality:", this.state.prevVisibality)
-}
+    let preVisibality = this.state.prevVisibality;
+    this.setState({ prevVisibality: !preVisibality });
+  };
+
 
   render() {
     const { curIdx, prevIdx } = this.state;
     const curStatus = prevIdx >= 0 ? EXAMPLE[curIdx].info : EXAMPLE[this.state.curPhaseIdx].info;
     const prevStatus = prevIdx >= 0 ? EXAMPLE[prevIdx].info : '';
-    
+    console.log("* curIdx:", curIdx + 1)
+    console.log("* prevIdx:", prevIdx + 1)
+    console.log("hererererere")
+    console.log("pre status:", prevStatus)
 
     return (
       <div>
@@ -94,10 +95,13 @@ export default class Timeline extends React.Component {
             }}
             index={this.state.curPhaseIdx}
             indexClick={(index) => {
-                this.setState({prevIdx: this.state.curIdx})
-                this.setState({curIdx: index})
-                this.toggleCurVisibility()
-                this.togglePreVisibility()
+              this.setState({ prevIdx: this.state.curIdx });
+              this.setState({ curIdx: index });
+              this.toggleCurVisibility();
+              this.togglePreVisibility();
+              console.log('current index', this.state.curIdx + 1);
+              console.log('pre index:', this.state.prevIdx + 1);
+              console.log('current phase:', this.state.curPhaseIdx + 1)
             }}
             values={EXAMPLE.map((x) => x.data)}
           />
@@ -105,22 +109,18 @@ export default class Timeline extends React.Component {
         <div>
           <Grid centered>
             <Transition animation="fly right" duration="1000" visible={this.state.curVisibality}>
-                <Card>
-                    <Card.Content>
-                        <h1>
-                            {curStatus}
-                        </h1>
-                    </Card.Content>
-                </Card>
+              <Card>
+                <Card.Content>
+                  <h1>{this.state.curVisibality?curStatus:prevStatus}</h1>
+                </Card.Content>
+              </Card>
             </Transition>
             <Transition animation="fly left" duration="1000" visible={this.state.prevVisibality}>
-                <Card>
-                    <Card.Content>
-                        <h1>
-                            {prevStatus}
-                        </h1>
-                    </Card.Content>
-                </Card>
+              <Card>
+                <Card.Content>
+                  <h1>{this.state.curVisibality?prevStatus:curStatus}</h1>
+                </Card.Content>
+              </Card>
             </Transition>
           </Grid>
         </div>
