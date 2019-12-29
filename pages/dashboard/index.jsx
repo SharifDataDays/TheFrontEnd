c6794f228a218acc7975cb2bd246d29c95b8e9d0
savaw <savaw97@gmail.com>
2019-12-29 13:39:24 +0330

    init

---
+++ pages/dashboard/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Navbar from '~/components/dashboard/navbar';
+
+export default class dashboard extends Component {
+  render() {
+    return (
+      <>
+        <Navbar transparent />
+      </>
+    );
+  }
+}
