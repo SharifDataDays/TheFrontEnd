159f3f84b0d6cd7aff9cc9a1053b3a6ef3dc4519
baharkhd <baharkh127@gmail.com>
2019-12-29 21:52:04 +0330

    pulling

--- components/dashboard/main/milestones.jsx
+++ components/dashboard/main/milestones.jsx
@@ -58,23 +58,41 @@ export default class Timeline extends React.Component {
   toggleCurVisibility = () => {
     let preVisibality = this.state.curVisibality;
     this.setState({ curVisibality: !preVisibality });
-    console.log('curVisibality:', this.state.curVisibality);
+
+    if (preVisibality){
+        setTimeout(
+            function() {
+              this.setState({ prevVisibality: !preVisibality });
+            }.bind(this),
+            1000,
+          );
+    }else{
+        this.setState({ prevVisibality: !preVisibality });
+    }
+
   };
 
   togglePreVisibility = () => {
     let preVisibality = this.state.prevVisibality;
-    this.setState({ prevVisibality: !preVisibality });
-  };
 
+    if (!preVisibality){
+        setTimeout(
+            function() {
+              this.setState({ prevVisibality: !preVisibality });
+            }.bind(this),
+            1000,
+          );
+    }else{
+        this.setState({ prevVisibality: !preVisibality });
+    }
+
+    
+  };
 
   render() {
     const { curIdx, prevIdx } = this.state;
     const curStatus = prevIdx >= 0 ? EXAMPLE[curIdx].info : EXAMPLE[this.state.curPhaseIdx].info;
     const prevStatus = prevIdx >= 0 ? EXAMPLE[prevIdx].info : '';
-    console.log("* curIdx:", curIdx + 1)
-    console.log("* prevIdx:", prevIdx + 1)
-    console.log("hererererere")
-    console.log("pre status:", prevStatus)
 
     return (
       <div>
@@ -99,29 +117,35 @@ export default class Timeline extends React.Component {
               this.setState({ curIdx: index });
               this.toggleCurVisibility();
               this.togglePreVisibility();
-              console.log('current index', this.state.curIdx + 1);
-              console.log('pre index:', this.state.prevIdx + 1);
-              console.log('current phase:', this.state.curPhaseIdx + 1)
             }}
             values={EXAMPLE.map((x) => x.data)}
           />
         </div>
         <div>
+            
           <Grid centered>
-            <Transition animation="fly right" duration="1000" visible={this.state.curVisibality}>
+              <Card>
+                  <Card.Content>
+                      {curStatus}
+                  </Card.Content>
+              </Card>
+            {/* <Transition animation="fly right" duration="1000" visible={this.state.curVisibality}>
               <Card>
                 <Card.Content>
-                  <h1>{this.state.curVisibality?curStatus:prevStatus}</h1>
+                    {
+                        console.log()
+                    }
+                  <h1>{this.state.curVisibality ? curStatus : prevStatus}</h1>
                 </Card.Content>
               </Card>
-            </Transition>
-            <Transition animation="fly left" duration="1000" visible={this.state.prevVisibality}>
+            </Transition> */}
+            {/* <Transition animation="fly left" duration="1000" visible={this.state.prevVisibality}>
               <Card>
                 <Card.Content>
-                  <h1>{this.state.curVisibality?prevStatus:curStatus}</h1>
+                  <h1>{this.state.curVisibality ? prevStatus : curStatus}</h1>
                 </Card.Content>
               </Card>
-            </Transition>
+            </Transition> */}
           </Grid>
         </div>
       </div>
