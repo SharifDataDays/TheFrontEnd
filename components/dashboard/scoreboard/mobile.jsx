76876366cfdee881e37b62e3dde379a98e583c19
pooyamoini <pooyamoini2000@gmail.com>
2019-12-31 01:28:25 +0330

    improvement in scoreboard-mobile

--- components/dashboard/scoreboard/mobile.jsx
+++ components/dashboard/scoreboard/mobile.jsx
@@ -6,6 +6,19 @@ const MyRow = styled(Menu)`
   border-bottom: 2px solid rgba(52, 136, 18, 0.25) !important;
 `;
 
+const MenuScoreBoard = styled(Menu)`
+  border-top: 2px solid rgba(34, 36, 38, 0.45);
+  border-bottom: 2px solid rgba(34, 36, 38, 0.45);
+`;
+
+const GenerateMyRow = ({ name, score, rank, display }) => (
+  <MyRow secondary style={{ display }}>
+    <Menu.Item position='right' name={score} />
+    <Menu.Item name={name} />
+    <Menu.Item name={rank} />
+  </MyRow>
+);
+
 const GenerateRows = (props) => {
   const rows = props.data.map((x) => {
     let background = '';
@@ -13,6 +26,8 @@ const GenerateRows = (props) => {
     if (rank <= 3) background = '#fed76673';
     else if (rank <= 6) background = '#bbbbbb73';
     else if (rank <= 9) background = '#cd7f3273';
+    if (x.first === props.myName)
+      return <GenerateMyRow name={x.first} score={x.second} rank={x.third} />;
     return (
       <Menu secondary pointing style={{ background }}>
         <Menu.Item position='right' name={x.second} />
@@ -39,28 +54,18 @@ const Footer = () => (
   </Table.Row>
 );
 
-const Scoreboard = (props) => {
+const Scoreboard = ({ data }) => {
+  const myName = 'پویا معینی';
+  const display = data.some((x) => x.first === myName) ? 'none' : '';
   return (
     <>
-      <Menu
-        fluid
-        secondary
-        pointing
-        style={{
-          borderTop: '2px solid rgba(34,36,38,.45)',
-          borderBottom: '2px solid rgba(34,36,38,.45)',
-        }}
-      >
+      <MenuScoreBoard fluid secondary pointing>
         <Menu.Item header position='right' name='امتیاز' />
         <Menu.Item header name='نام' />
         <Menu.Item header name='رتبه' />
-      </Menu>
-      <MyRow secondary>
-        <Menu.Item position='right' name='80' />
-        <Menu.Item name='پویا معینی' />
-        <Menu.Item name='3' />
-      </MyRow>
-      <GenerateRows data={props.data} />
+      </MenuScoreBoard>
+      <GenerateMyRow name={myName} score='100' rank='12' display={display} />
+      <GenerateRows data={data} myName={myName} />
       <Footer />
     </>
   );
