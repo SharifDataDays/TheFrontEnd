5ddc7c0c08063e51d6b08815aba2509dee21e1f1
parsa alian <parsaalian0@gmail.com>
2020-01-11 15:39:56 +0330

    minor changes

--- redux/api/index.js
+++ redux/api/index.js
@@ -1,5 +1,5 @@
-const BASE_ADDR = 'https://datadays.sharif.edu/test/api';
-export const BASE = 'https://datadays.sharif.edu'
+const BASE_ADDR = 'http://37.152.190.92/test/api';
+export const BASE = 'http://37.152.190.92';
 
 export function termsAPI() {
   return `${BASE_ADDR}/terms`;
