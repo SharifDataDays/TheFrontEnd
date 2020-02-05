6d07b8ba5936994aae6a4d5cdd1b1ee2341b4e3a
savaw <savaw97@gmail.com>
2020-02-05 12:02:56 +0330

    Edit team page

--- components/dashboard/team/container.jsx
+++ components/dashboard/team/container.jsx
@@ -1,20 +1,15 @@
 import _ from 'lodash';
 import styled from 'styled-components';
-import { space, layout, color, border, typography, flex } from 'styled-system';
-import { Header, List, Input, Divider, Button, Grid, Form, Message } from 'semantic-ui-react';
-import { Fade } from 'react-reveal';
+import { space, color, border} from 'styled-system';
+import TeamInfo from './teamInfo';
 
-const Label = styled.label`
-  ${space}
-  ${layout}
-  ${typography}
-`;
 
 const Container = styled.div`
   ${space}
   ${color}
   ${border}
 `;
+
 
 export default function ProfileContainer({ token, teamData }) {
   const finilized = false;
@@ -25,71 +20,8 @@ export default function ProfileContainer({ token, teamData }) {
       py={5}
       m={0}
       backgroundColor="rgba(255, 255, 255, 0.3)"
-      borderRadius={6}
     >
-      <Header size="huge" dir="RTL">
-        تیم من
-      </Header>
-      <Divider />
-
-      <Fade up>
-        <Grid dir="RTL">
-          <Grid.Column
-            verticalAlign="middle"
-            style={{
-              marginTop: '20px',
-         
-              border: '1px solid #d1d1d1',
-              borderRadius: '10px'
-            }}
-            
-          >
-            <Grid.Row>
-              <Label pl={4}>نام تیم: </Label>
-              <Input
-                placeholder="نام تیم"
-                defaultValue={teamData.name}
-                disabled={finilized}
-                transparent={finilized}
-              />
-            </Grid.Row>
-            <Grid.Row
-              style={{
-                paddingTop: '15px',
-              }}
-            >
-              اعضای تیم:
-              <List bulleted>
-                {_.map(teamData.participants, (user) => {
-                  return (
-                    <List.Item
-                      key={user.id}
-                      style={{
-                        padding: '10px',
-                      }}
-                    >
-                      {user.user.username} | {user.user.email}
-                    </List.Item>
-                  );
-                })}
-              </List>
-              <Message hidden={true} positive>
-                تغییرات با موفقیت ذخیره شد.
-              </Message>
-              <Message hidden={true} negative>
-                تغییرات بدون موفقیت ذخیره شد.
-              </Message>
-              <Form.Button
-                primary
-                content="ذخیره‌ی تغییرات"
-                floated="right"
-                size="small"
-                disabled={finilized}
-              />
-            </Grid.Row>
-          </Grid.Column>
-        </Grid>
-      </Fade>
+    <TeamInfo teamData={teamData} token={token} finilized={finilized}/>
     </Container>
   );
 }
---
+++ components/dashboard/team/teamInfo.jsx
@@ -0,0 +1,169 @@
+import _ from 'lodash';
+import styled from 'styled-components';
+import { space, layout, color, border, typography, flex } from 'styled-system';
+import {
+  Header,
+  List,
+  Input,
+  Divider,
+  Button,
+  Grid,
+  Form,
+  Message,
+  Popup,
+} from 'semantic-ui-react';
+import { Fade } from 'react-reveal';
+
+const Label = styled.label`
+  ${space}
+  ${layout}
+  ${typography}
+  ${color}
+`;
+
+const Container = styled.div`
+  ${space}
+  ${color}
+  ${border}
+`;
+
+const Text = styled.text`
+  ${color}
+`;
+
+export default function TeamInfo({ token, teamData, finilized }) {
+
+  return (
+    <>
+      <Container
+        style={{
+          display: 'flex',
+          flexDirection: 'row-reverse',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          paddingBottom: '20px'
+        }}
+      >
+        <Header size="huge" dir="RTL" floated="left">
+          تیم من
+        </Header>
+        <Popup
+          dir="RTL"
+          position="right center"
+          content="پس از ثبت نهایی امکان تغییر نام و اعضا وجود ندارد!"
+          inverted
+          trigger={
+            <Form.Button
+              primary
+              inverted
+              content="ثبت نهایی"
+              floated="right"
+              size="big"
+              disabled={finilized}
+            />
+          }
+        />
+      </Container>
+
+      <Fade up>
+        <Grid dir="RTL">
+          <Grid.Column
+            verticalAlign="middle"
+            style={{
+              marginTop: '20px',
+
+              border: '1px solid #d1d1d1',
+              borderRadius: '10px',
+            }}
+          >
+            <Grid.Row>
+              <Container
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Container>
+                  <Label primary pl={4} style={{ fontWeight: 'bold' }}>
+                    نام تیم:
+                  </Label>
+                  <Input
+                    placeholder="نام تیم"
+                    defaultValue={teamData.name}
+                    disabled={finilized}
+                    transparent={finilized}
+                  />
+                </Container>
+                <Form.Button
+                  primary
+                  content="تغییر نام"
+                  floated="right"
+                  size="small"
+                  disabled={finilized}
+                />
+              </Container>
+              <Divider />
+            </Grid.Row>
+            <Grid.Row>
+              <Label primary pl={4} style={{ fontWeight: 'bold' }}>
+                اعضای تیم:
+              </Label>
+              <List bulleted>
+                {_.map(teamData.participants, (user) => {
+                  return (
+                    <List.Item
+                      key={user.id}
+                      style={{
+                        padding: '10px',
+                      }}
+                    >
+                      {user.user.first_name} {user.user.last_name} ({user.user.username})
+                    </List.Item>
+                  );
+                })}
+              </List>
+            </Grid.Row>
+            <Grid.Row hidden={finilized}>
+            <Divider />
+
+              <List>
+                <Label primary pl={4} style={{ fontWeight: 'bold' }}>
+                  اضافه کردن عضو جدید:
+                </Label>
+                <List.Item>
+                  <Container
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      justifyContent: 'space-between',
+                    }}
+                  >
+                    <Input placeholder="نام کاربری" disabled={finilized} transparent={finilized} />
+                    <Form.Button
+                      primary
+                      content="اضافه کردن"
+                      floated="right"
+                      size="small"
+                      disabled={finilized}
+                    />
+                  </Container>
+                </List.Item>
+              </List>
+            </Grid.Row>
+            <Grid.Row>
+              <Message hidden={true} positive>
+                تغییرات با موفقیت ذخیره شد.
+              </Message>
+              <Message hidden={true} negative>
+                تغییرات بدون موفقیت ذخیره شد.
+              </Message>
+            </Grid.Row>
+          </Grid.Column>
+        </Grid>
+      </Fade>
+    </>
+  );
+}
