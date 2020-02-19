165349e3f03b5889da2626d93e7a910194dd202d
savaw <savaw97@gmail.com>
2020-02-19 21:29:44 +0330

    Edit count down

--- components/home/countdown.jsx
+++ components/home/countdown.jsx
@@ -4,40 +4,40 @@ import persianJs from 'persianjs';
 import { space, color, typography } from 'styled-system';
 
 const Container = styled.div`
-${space}
-display:flex;
-flex-direction:column;
-background-color:#191919;
+  ${space}
+  display:flex;
+  flex-direction: column;
+  background-color: #191919;
 `;
 
 const Row = styled.div`
-${space}
-display: flex;
-justify-content: center;
+  ${space}
+  display: flex;
+  justify-content: center;
 `;
 
 const TimeBlock = styled.div`
-margin: 0px 2vh;
-display:flex;
-flex-direction:column;
-align-items:center;
-justify-content:center;
+  margin: 0px 2vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const TimeWrapper = styled.div`
-background-color:white;
-padding: 3vh;
-width:13vmin;
-border-radius:10px;
-border: 2px solid #0088fb;
-display:flex;
-align-items:center;
-justify-content:center;
+  background-color: rgba(0, 0, 0, 0.3);
+  padding: 3vh;
+  width: 13vmin;
+  border-radius: 10px;
+  border: 2px solid #0088fb;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Time = styled.div`
-color:#0088fb;
-font-size:6vmin;
+  color: #0088fb;
+  font-size: 6vmin;
 `;
 
 const Label = styled.div`
@@ -70,10 +70,18 @@ class CountDown extends Component {
       const now = new Date().getTime();
       const distance = target - now;
       this.setState({
-        day: persianJs(Math.floor(distance / (day)).toString()).englishNumber().toString(),
-        hour: persianJs(Math.floor((distance % (day)) / (hour)).toString()).englishNumber().toString(),
-        minute: persianJs(Math.floor((distance % (hour)) / (minute)).toString()).englishNumber().toString(),
-        second: persianJs(Math.floor((distance % (minute)) / second).toString()).englishNumber().toString(),
+        day: persianJs(Math.floor(distance / day).toString())
+          .englishNumber()
+          .toString(),
+        hour: persianJs(Math.floor((distance % day) / hour).toString())
+          .englishNumber()
+          .toString(),
+        minute: persianJs(Math.floor((distance % hour) / minute).toString())
+          .englishNumber()
+          .toString(),
+        second: persianJs(Math.floor((distance % minute) / second).toString())
+          .englishNumber()
+          .toString(),
       });
     }, second);
   }
@@ -83,28 +91,44 @@ class CountDown extends Component {
     return (
       <>
         <Container p={5}>
-          <Row
-            pb={4}
-          >
+          <Row pb={4}>
             <TimeBlock>
-              <TimeWrapper><Time>{day}</Time></TimeWrapper>
-              <Label my={3} fontSize={4} color={'#9a9a9a'}>روز</Label>
+              <TimeWrapper>
+                <Time>{day}</Time>
+              </TimeWrapper>
+              <Label my={3} fontSize={4} color={'#9a9a9a'}>
+                روز
+              </Label>
             </TimeBlock>
             <TimeBlock>
-              <TimeWrapper><Time>{hour}</Time></TimeWrapper>
-              <Label my={3} fontSize={4} color={'#9a9a9a'}>ساعت</Label>
+              <TimeWrapper>
+                <Time>{hour}</Time>
+              </TimeWrapper>
+              <Label my={3} fontSize={4} color={'#9a9a9a'}>
+                ساعت
+              </Label>
             </TimeBlock>
             <TimeBlock>
-              <TimeWrapper><Time>{minute}</Time></TimeWrapper>
-              <Label my={3} fontSize={4} color={'#9a9a9a'}>دقیقه</Label>
+              <TimeWrapper>
+                <Time>{minute}</Time>
+              </TimeWrapper>
+              <Label my={3} fontSize={4} color={'#9a9a9a'}>
+                دقیقه
+              </Label>
             </TimeBlock>
             <TimeBlock>
-              <TimeWrapper><Time>{second}</Time></TimeWrapper>
-              <Label my={3} fontSize={4} color={'#9a9a9a'}>ثانیه</Label>
+              <TimeWrapper>
+                <Time>{second}</Time>
+              </TimeWrapper>
+              <Label my={3} fontSize={4} color={'#9a9a9a'}>
+                ثانیه
+              </Label>
             </TimeBlock>
           </Row>
           <Row>
-            <Label fontSize={6} color={'white'} lineHeight='1.5'>تا پایان مهلت ثبت تیم</Label>
+            <Label fontSize={[5,6,6]} color={'white'} lineHeight="1.5">
+              تا پایان مهلت ثبت تیم
+            </Label>
           </Row>
         </Container>
       </>
